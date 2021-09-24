49bb63be40972b6780de0241bfe308437a2c5c71
Hoshhh <nyxious123@live.com>
2021-09-23 21:52:48 -0400

    Setup the API to fetch data

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -3,11 +3,38 @@ import millify from 'millify';
 import { Typography, Row, Col, Statistic } from 'antd';
 import { Link } from 'react-router-dom';
 
+import { useGetCryptosQuery } from '../services/cryptoAPI';
+import { Cryptocurrencies, News } from '../components'
+
+const { Title } = Typography;
+
 const Homepage = () => {
+    const { data, isFetching } = useGetCryptosQuery();
+    const globalStats = data?.data?.stats;
+
+    if (isFetching) return 'Loading... ';
+
     return (
-        <div>
-            Homepage
-        </div>
+        <>
+            <Title level={2} className="heading">Global Crypto Stats</Title>
+            <Row>
+                <Col span={12}><Statistic title="Total Cryptocurrencies" value={globalStats.total}/></Col>
+                <Col span={12}><Statistic title="Total Exchanges" value={millify(globalStats.totalExchanges)}/></Col>
+                <Col span={12}><Statistic title="Total Market Cap" value={millify(globalStats.totalMarketCap)}/></Col>
+                <Col span={12}><Statistic title="Total 24h Volume" value={millify(globalStats.total24hVolume)}/></Col>
+                <Col span={12}><Statistic title="Total MArkets" value={millify(globalStats.totalMarkets)}/></Col>
+            </Row>
+            <div className="home-heading-container">
+                <Title level={2} className="home-title">Top 10 Cyptocurrencies in the world</Title>
+                <Title level={3} className="show-more"><Link to="/cryptocurrencies">Show More</Link></Title>
+            </div>
+            <Cryptocurrencies simplified/>
+            <div className="home-heading-container">
+                <Title level={2} className="home-title">Latest Cypto News</Title>
+                <Title level={3} className="show-more"><Link to="/news">Show More</Link></Title>
+            </div>
+            <News simplified/>
+        </>
     )
 }
 
---
+++ src/services/cryptoAPI.js
@@ -0,0 +1,23 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+const cryptoApiHeaders = {
+    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+    'x-rapidapi-key': '6415a36ce3msh79301d8b94eb4ccp18dc97jsn93df05cdd88b'
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders })
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({baseUrl}),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: () => createRequest('/coins')
+        })
+    })
+})
+
+export const {
+    useGetCryptosQuery
+} = cryptoApi;
